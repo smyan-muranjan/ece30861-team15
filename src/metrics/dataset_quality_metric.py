0ead156f2c6bfa0b0ad453b5cfa6e0607b671148
smuranjan1 <smyan.muranjan@gmail.com>
2025-09-14 20:25:06 -0400

    Fix linting error.

--- src/metrics/dataset_quality_metric.py
+++ src/metrics/dataset_quality_metric.py
@@ -7,7 +7,7 @@ from src.models.dataset_stats import DatasetStats
 class DatasetQualityMetric(Metric):
     LIKES_WEIGHT = 0.5
     DOWNLOADS_WEIGHT = 0.5
-    
+
     def calculate(self, repository_data: Any) -> float:
         assert isinstance(repository_data, DatasetStats)
         return self.LIKES_WEIGHT * repository_data.normalized_likes + \
