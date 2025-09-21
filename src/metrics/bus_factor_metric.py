b7ddc2053ca4498fa6be57a555a2817aa4b0451e
Jadden Picardal <jaddenki@gmail.com>
2025-09-21 08:56:24 -0400

    remove whitespace...

--- src/metrics/bus_factor_metric.py
+++ src/metrics/bus_factor_metric.py
@@ -7,7 +7,7 @@ from src.metrics.metric import Metric
 class BusFactorMetric(Metric):
     def __init__(self, git_client: Optional[GitClient] = None):
         self.git_client = git_client or GitClient()
-    
+
     async def calculate(self, metric_input: Any) -> float:
         assert isinstance(metric_input, str)
 
